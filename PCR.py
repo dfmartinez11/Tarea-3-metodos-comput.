a607b66520816e793cea00dab8ae3bb65728e5c1
dfmartinez11 <42391122+dfmartinez11@users.noreply.github.com>
2018-10-24 20:01:18 -0500

    Add files via upload

--- PCR.py
+++ PCR.py
@@ -27,8 +27,37 @@ for i in  lineas:
 
 #print total[0]
 matriz = np.array(total)
-Matriz = matriz.transpose()[0]
-#print Matriz
+Matriz = matriz.transpose()
+
+#print Matriz[1]
+#print Matriz.shape[0]
+
+var=[]
+for i in range(Matriz.shape[0]):
+	prom = np.mean(Matriz[i,:])
+	varr = 0.0
+	for j in range(Matriz.shape[1]):
+		Matriz[i,j] = (Matriz[i,j]- prom)/(0.1*(Matriz.shape[1]-1) )
+		varr += (Matriz[i,j]**2)
+	var.append(varr**0.5)
+	for j in range(Matriz.shape[1]):
+		Matriz[i,j] = Matriz[i,j]/(varr**0.5)
+		
+
+covar = np.cov(Matriz)
+print "---------fila 0 real"
+print covar[0:2]
+
+covar=np.ones((Matriz.shape[0] , Matriz.shape[0] ))
+for i in range(Matriz.shape[0]):
+	for j in range(Matriz.shape[0]):
+		covar[i,j] = np.dot( Matriz[i,:],Matriz[j,:] )
+		
+
+print "---------fila 0 implementada"
+print covar[0:2]
+
+	
 
 	
 	
